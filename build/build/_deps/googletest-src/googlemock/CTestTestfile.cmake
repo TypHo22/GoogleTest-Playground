824ce228ab41d7500f174175614d0fad81bb386b
TypHo22 <42981587+TypHo22@users.noreply.github.com>
2022-02-08 21:44:54 +0100

    removed build folder

--- build/build/_deps/googletest-src/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zaragesh/develop/GoogleTestPlayground/build/_deps/googletest-src/googlemock
-# Build directory: /home/zaragesh/develop/GoogleTestPlayground/build/build/_deps/googletest-src/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/build/_deps/googletest-src/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zaragesh/develop/GoogleTestPlayground/build/_deps/googletest-src/googletest
-# Build directory: /home/zaragesh/develop/GoogleTestPlayground/build/build/_deps/googletest-src/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
